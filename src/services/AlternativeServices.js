6113a3514d9a79e6420a4b850857333b46bf9a91
nathan-nando <nathan.nandoo@gmail.com>
2023-04-08 09:21:13 +0700

    create alternative

---
+++ src/services/AlternativeServices.js
@@ -0,0 +1,26 @@
+import { api } from "@/api/config";
+import { endpoint } from "@/api/endpoint";
+
+class AlternativeServices {
+  getAll() {
+    return api.get(`${endpoint.alternative}`);
+  }
+
+  getByID(id) {
+    return api.get(`${endpoint.alternative}/${id}`);
+  }
+
+  create(payload) {
+    return api.post(endpoint.alternative, payload);
+  }
+
+  update(payload) {
+    return api.patch(endpoint.alternative, payload);
+  }
+
+  delete(id) {
+    return api.delete(endpoint.alternative + `/${id}`);
+  }
+}
+
+export default new AlternativeServices();
